d317d7019f5e8f003ec0273d27a283725c3f0957
Lucas Dias <lucas.dias1987lu@gmail.com>
2022-10-24 19:29:26 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -40,19 +40,13 @@ function carousel() {
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-  var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
+function clicMenu(){
+  if(itens.style.display =='block'){
+      itens.style.display ='none';
+    
   }
-}
-
-// When the user clicks anywhere outside of the modal, close it
-var modal = document.getElementById('ticketModal');
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  else
+  {
+      itens.style.display ='block';
   }
-}
+  }
